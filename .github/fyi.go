d217e7d3202900f51b1a3ab78624f64de1dd4094
Charchit <untamedcharchit@gmail.com>
2025-05-23 23:10:34 +0530

    fix

---
+++ .github/fyi.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	http.NotFound(w, r)
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	books = append(books, book)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func main() {
+	// Sample data
+	books = append(books, Book{ID: "1", Title: "1984", Author: "George Orwell"})
+	books = append(books, Book{ID: "2", Title: "The Go Programming Language", Author: "Alan Donovan"})
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	fmt.Println("Server running at http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
